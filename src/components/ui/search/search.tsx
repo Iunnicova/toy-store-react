1a76f9b1fce707b68e89b1bcfd2b5b5ac1c77496
Valentina <yunnicova@gmail.com>
2025-10-03 20:53:29 +0200

    feat: add search laptop-mobile-s

--- src/components/ui/search/search.tsx
+++ src/components/ui/search/search.tsx
@@ -6,14 +6,12 @@ import { Button } from '../button';
 export const Search = () => (
   <section className={styles.search}>
     <h2 className={styles.searchTitle}>Найди свою любимку!</h2>
-
     <div className={styles.searchBox}>
       <input
         className={styles.input}
         type="search"
         placeholder="Напиши моё имя"
       />
-
       <NavLink to="/">
         <Button onClick={() => alert('Поиск')} className={styles.button}>
           <img src={searchIcon} alt="Поиск" className={styles.icon} />
