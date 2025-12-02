0755515e055a2e4c9816dfd0a315691b2706e620
Valentina <yunnicova@gmail.com>
2025-12-02 18:42:17 +0100

    fix: bottom

--- src/components/pages/FavoritesPage/FavoritesPage.tsx
+++ src/components/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,8 +1,13 @@
+import styles from './FavoritesPage.module.scss';
+
 export const FavoritesPage = () => {
   return (
-    <div style={{ padding: '40px 20px', textAlign: 'center' }}>
-      <h1>Избранное</h1>
-      <p>Тут скоро будут ваши любимые игрушки</p>
+    <div className={styles.favorites}>
+      <h1>Вы ещё не добавили товары в избранное.</h1>
+      <p>
+        Просто нажмите ❤️, чтобы сохранить понравившиеся и вернуться к ним
+        позже.
+      </p>
     </div>
   );
 };
