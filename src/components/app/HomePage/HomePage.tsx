5f479d3f305139e02c0d8c17fe81dc4ebe2fe6b9
Valentina <yunnicova@gmail.com>
2025-12-14 14:24:03 +0100

    feat: add connection db.json

--- src/components/app/HomePage/HomePage.tsx
+++ src/components/app/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TToy } from '../../../types/toysData';
 import {
   Banner,
@@ -14,6 +14,25 @@ import styles from './HomePage.module.scss';
 export const HomePage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedToy, setSelectedToy] = useState<TToy | null>(null);
+
+///добавляем карточки через db.jso
+const [toys, setToys] = useState<TToy[]>([]);        // состояние игрушек
+  const [loading, setLoading] = useState(true);  // loading — текущее значение (true/false) setLoading — функция, которая меняет это значение
+
+useEffect(() => {
+    fetch('http://localhost:3001/toys')
+      .then(res => res.json())
+      .then(data => {
+        setToys(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error('Ошибка загрузки игрушек:', err);
+        setLoading(false);  // даже при ошибке скрываем лоадер
+      });
+  }, []);
+
+//******* */
 
   const handleOpenModal = (toy: TToy) => {
     setSelectedToy(toy);
